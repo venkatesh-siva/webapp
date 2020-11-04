392b6236505d96672e5d7c91aada36d414ffbbc8
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-11-04 05:37:38 -0500

    adding dev/null command to appstart

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start the Spring boot app running
-sudo nohup java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar >> app.log 2>&1 &
+java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
