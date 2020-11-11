88df0aaefc427844ae42734f55a10319d69f2064
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-11-11 18:54:29 -0500

    appication start changes and adding logback-spring file

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # Start the Spring boot app running
-nohup java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
+java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
-
-crontab -l > mycron
-echo "@reboot java -jar /home/ubuntu/csye/target/csye-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &" >> mycron
-crontab mycron
-rm mycron
