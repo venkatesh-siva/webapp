0b0b4a6a5ee9614ae50d23b0208ea0135fbdf154
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-11-11 22:47:29 -0500

    adding nohup

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 # Start the Spring boot app running
-java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-
+nohup java -jar ~/csye/target/csye-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
