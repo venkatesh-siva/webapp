96492fe0599884036aef1de2d8bfe32d9e332800
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-11-12 09:30:22 -0500

    removing create log file script

--- codedeploy/applicationstart.sh
+++ codedeploy/applicationstart.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Start the Spring boot app running
-sudo mkdir /home/ubuntu/logs
-sudo touch /home/ubuntu/logs/csye6225.log
-sudo chmod 666 /home/ubuntu/logs/csye6225.log
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
 sudo java -jar /home/ubuntu/csye/target/csye-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapp.log 2> /home/ubuntu/webapp.log < /home/ubuntu/webapp.log &
