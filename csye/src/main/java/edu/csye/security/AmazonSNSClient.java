19d4ec174b1a08e52d5ab915b2593d8453947875
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-12-01 17:56:55 -0500

    fixing sns arn

--- csye/src/main/java/edu/csye/security/AmazonSNSClient.java
+++ csye/src/main/java/edu/csye/security/AmazonSNSClient.java
@@ -26,7 +26,7 @@ public class AmazonSNSClient {
     }
 
     public void publish(String message) {
-        final PublishRequest publishRequest = new PublishRequest("arn:aws:sns:us-east-1:371394122941:password-reset", message);
+        final PublishRequest publishRequest = new PublishRequest("arn:aws:sns:us-east-1:599351014538:email-customer", message);
         logger.info("AmazonSNSClientClass- Published Request : " + publishRequest.toString() + "----");
         final PublishResult publishResponse = snsClient.publish(publishRequest);
         logger.info("AmazonSNSClientClass- Published message with messageId :- " + publishResponse.getMessageId());
