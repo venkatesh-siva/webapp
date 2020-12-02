78c3e60b91a07e093a1821413778f7f1af64ad8c
venkatesh-siva <sivakumar.ve@northeastern.edu>
2020-12-02 16:24:55 -0500

    modifying message for duplicate check

--- csye/src/main/java/edu/csye/security/AmazonSNSClient.java
+++ csye/src/main/java/edu/csye/security/AmazonSNSClient.java
@@ -1,32 +1,39 @@
 package edu.csye.security;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.CreateTopicResult;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
 
+
+
 @Service("amazonSNSService")
 public class AmazonSNSClient {
-    private AmazonSNS snsClient;
-
-    private Logger logger = LoggerFactory.getLogger(AmazonSNSClient.class);
-
-    @PostConstruct
-    private void init() {
-        this.snsClient = AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance()).build();
-    }
+	@Value("${AWS_REGION}")
+	private String region;
+	private AmazonSNS snsClient;
+	private CreateTopicResult topic;
+	private final static Logger logger = LoggerFactory.getLogger(AmazonSNSClient.class);
+	String topicArn;
+	@Autowired
+	public AmazonSNSClient(){
+		
+	    InstanceProfileCredentialsProvider provider = new InstanceProfileCredentialsProvider(true);
+	    this.snsClient =  AmazonSNSClientBuilder.standard().withCredentials(provider).withRegion(region).build();
+	    this.topic = snsClient.createTopic("TOPIC_EMAIL");
+	    topicArn=topic.getTopicArn();
+	}
 
     public void publish(String message) {
-        final PublishRequest publishRequest = new PublishRequest("arn:aws:sns:us-east-1:599351014538:email-customer", message);
+        final PublishRequest publishRequest = new PublishRequest(topicArn, message);
         logger.info("AmazonSNSClientClass- Published Request : " + publishRequest.toString() + "----");
         final PublishResult publishResponse = snsClient.publish(publishRequest);
         logger.info("AmazonSNSClientClass- Published message with messageId :- " + publishResponse.getMessageId());
